20a18e4d637509ef72f89f1d0d8c0f763053901a
TechNomad4 <kamaudavis3022@gmail.com>
2023-11-03 14:51:12 +0300

    Updates

---
+++ precache-manifest.0d7c49654c0913d0912b8defed91c305.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fe33b816fa2dd680d2b1042009223e",
+    "url": "/MoviesApp/index.html"
+  },
+  {
+    "revision": "059d38e5e997989da9d7",
+    "url": "/MoviesApp/static/css/main.7b3c70f0.chunk.css"
+  },
+  {
+    "revision": "50150b106a868941696b",
+    "url": "/MoviesApp/static/js/2.3cf7b9e5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MoviesApp/static/js/2.3cf7b9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "059d38e5e997989da9d7",
+    "url": "/MoviesApp/static/js/main.ea7c299b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/MoviesApp/static/js/main.ea7c299b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8773e198a80bde592d68",
+    "url": "/MoviesApp/static/js/runtime-main.f5bae05c.js"
+  }
+]);
